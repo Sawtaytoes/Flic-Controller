c702f9dc8a3a7204027290b5af7fc05124d36584
Kevin Ghadyani <Saturn2888@gmail.com>
2017-06-26 05:47:42 -0500

    Setup sample button presses

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
 const fetch = require('node-fetch')
 
-const fliclib = require("./fliclibNodeJs");
-const FlicClient = fliclib.FlicClient;
-const FlicConnectionChannel = fliclib.FlicConnectionChannel;
-const FlicScanner = fliclib.FlicScanner;
+const fliclib = require("./fliclibNodeJs")
+const FlicClient = fliclib.FlicClient
+const FlicConnectionChannel = fliclib.FlicConnectionChannel
+const FlicScanner = fliclib.FlicScanner
 
-const client = new FlicClient("apple-pi.kevinghadyani.com", 5551);
+const client = new FlicClient("apple-pi.kevinghadyani.com", 5551)
 
 const buttonConfigs = {
-	'80:e4:da:72:9d:27': () => fetch('http://tablettron:30061/toggle-scene/Ashlee\'s Vanity')
+	'80:e4:da:72:9d:27': {
+		SingleClick: () => fetch('http://lifx.kevinghadyani.com:36001/toggle-scene/Shower'),
+		DoubleClick: () => fetch('http://lifx.kevinghadyani.com:36001/toggle-scene/Late Night Bathroom'),
+		Hold: () => fetch('http://lifx.kevinghadyani.com:36001/toggle-group/Master Bathroom'),
+	}
 }
 
-const listenToButton = (bdAddr) => {
-	const cc = new FlicConnectionChannel(bdAddr);
-	client.addConnectionChannel(cc);
-	cc.on("buttonUpOrDown", (...args) => {
-		buttonConfigs[bdAddr]()
+const listenToButton = (bluetoothAddress) => {
+	const cc = new FlicConnectionChannel(bluetoothAddress)
+
+	client.addConnectionChannel(cc)
+	cc.on("buttonSingleOrDoubleClickOrHold", clickType => {
+		console.log('clickType', clickType)
+
+		buttonConfigs[bluetoothAddress][clickType.replace('Button', '')]()
 		.then(() => console.log('Command Executed Successfully'))
 		.catch(err => console.error(err))
-		console.log(args);
-	});
-	// cc.on("buttonUpOrDown", (clickType, wasQueued, timeDiff) => {
-	// 	console.log(bdAddr + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
-	// });
+	})
+	cc.on("buttonUpOrDown", (clickType, wasQueued, timeDiff) => {
+		console.log(bluetoothAddress + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago")
+	})
 	cc.on("connectionStatusChanged", (connectionStatus, disconnectReason) => {
-		console.log(bdAddr + " " + connectionStatus + (connectionStatus == "Disconnected" ? " " + disconnectReason : ""));
-	});
+		console.log(bluetoothAddress + " " + connectionStatus + (connectionStatus == "Disconnected" ? " " + disconnectReason : ""))
+	})
 }
 
 client.once("ready", function() {
-	console.log("Connected to daemon!");
-	client.getInfo(function(info) {
-		info.bdAddrOfVerifiedButtons.forEach(function(bdAddr) {
-			listenToButton(bdAddr);
-		});
-	});
-});
+	console.log("Connected to daemon!")
+
+	client.getInfo(info => (
+		info.bdAddrOfVerifiedButtons.forEach(
+			bluetoothAddress => listenToButton(bluetoothAddress)
+		)
+	))
+})
 
 client.on("bluetoothControllerStateChange", function(state) {
-	console.log("Bluetooth controller state change: " + state);
-});
+	console.log("Bluetooth controller state change: " + state)
+})
 
 client.on("newVerifiedButton", function(bdAddr) {
-	console.log("A new button was added: " + bdAddr);
-	listenToButton(bdAddr);
-});
+	console.log("A new button was added: " + bdAddr)
+	listenToButton(bdAddr)
+})
 
 client.on("error", function(error) {
-	console.log("Daemon connection error: " + error);
-});
+	console.log("Daemon connection error: " + error)
+})
 
 client.on("close", function(hadError) {
-	console.log("Connection to daemon is now closed");
-});
+	console.log("Connection to daemon is now closed")
+})
