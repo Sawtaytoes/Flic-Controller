13e4a7256f2a327a10bbdd781dbef1cbd10e9f75
Kevin Ghadyani <Saturn2888@gmail.com>
2017-06-26 00:07:51 -0500

    Removed LIFX Controller middleware

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ global.baseDir = `${__dirname}/`
 
 // Load Config settings
 const dir = require(`${global.baseDir}/global-dirs`)
-const lifxConfig = require(`${dir.services}setup-lifx-config`)
-const lifxClient = require(`${dir.services}setup-lifx-client`)
+const flicClient = require(`${dir.services}setup-flic-client`)
 const setupServer = require(`${dir.server}setup-server`)
 const startServer = require(`${dir.server}start-server`)
 
 // Load Middleware
 const discoverDevices = require(`${dir.middleware}discover-devices`)
-const toggleGroup = require(`${dir.middleware}toggle-group`)
-const toggleScene = require(`${dir.middleware}toggle-scene`)
 
-lifxClient.init()
-lifxConfig.init()
+flicClient.init()
 
-const lifxMiddleware = {
+const flicMiddleware = {
 	get: action => (req, res) => res.send(
-		action(lifxClient, lifxConfig)(req.params.name)
+		action(flicClient)(req.params.name)
 	)
 }
 
@@ -31,17 +27,7 @@ serverSettings.get(
 
 serverSettings.get(
 	'/discover-devices',
-	lifxMiddleware.get(discoverDevices)
-)
-
-serverSettings.get(
-	'/toggle-group/:name',
-	lifxMiddleware.get(toggleGroup)
-)
-
-serverSettings.get(
-	'/toggle-scene/:name',
-	lifxMiddleware.get(toggleScene)
+	flicMiddleware.get(discoverDevices)
 )
 
 startServer(serverSettings)
