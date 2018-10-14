1cc5f149debd37987cc4932dc8d61a00ae7b4aac
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-14 06:24:02 -0500

    Added node-fetch to epicMiddleware dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Include this import before other local imports.
 require('@ghadyani-framework/setup-module-aliases')(__dirname)
 
+const fetch = require('node-fetch')
 const { applyMiddleware, createStore } = require('redux')
 const { createActionLoggerMiddleware } = require('@ghadyani-framework/redux-utils')
 const { createConfigurationSet, runTasks } = require('@ghadyani-framework/node')
@@ -22,7 +23,13 @@ const actionLoggerMiddleware = (
 	})
 )
 
-const epicMiddleware = createEpicMiddleware()
+const epicMiddleware = (
+	createEpicMiddleware({
+		dependencies: {
+			fetch,
+		},
+	})
+)
 
 const middleware = (
 	applyMiddleware(
